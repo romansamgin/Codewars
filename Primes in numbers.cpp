ae2b80ab509f334fa56915e8d95a61026a19dd1a
Roman Samgin <40915074+spprssr@users.noreply.github.com>
2020-08-21 16:05:30 +0300

    Add files via upload

---
+++ Primes in numbers.cpp
@@ -0,0 +1,77 @@
+#include <vector>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+bool isPrime(int n)
+{
+    int i = 2;
+    while (i * i <= n) 
+    {
+        if (!(n % i))
+            return false;
+        i++;
+    }
+    return true;
+}
+
+
+int deg(int n, int p)
+{
+    if (n % p)
+        return 0;
+    int k(0);
+    while (!(n % p))
+    {
+        k++;
+        n /= p;
+    }
+    return k;
+}
+
+class PrimeDecomp
+{
+public:
+    static std::string factors(int lst);
+
+};
+
+string PrimeDecomp::factors(int lst)
+{
+    if (isPrime(lst))
+        return "(" + to_string(lst) + ")";
+    vector<pair<int, int>>v{};    
+    if (!(lst % 2))
+    {             
+        v.push_back(make_pair(2, deg(lst, 2)));       
+    }      
+    vector<int>ans{};
+    for(int i = 3; i < lst/2+1; i+=2)
+    {
+        if (!(lst % i))
+            ans.push_back(i);
+    }
+      
+    for (auto i : ans)
+    {                
+        if (isPrime(i))
+        {
+            v.push_back(make_pair(i, deg(lst, i)));
+        }          
+    }
+
+    string s = "";
+    for (auto temp : v)
+    {
+        if (temp.second == 1)
+        {
+            s += "(" + to_string(temp.first) + ")";
+        }
+        else
+        {
+            s += "(" + to_string(temp.first) + "**" + to_string(temp.second) + ")";
+        }
+    }
+    return s;
+}
