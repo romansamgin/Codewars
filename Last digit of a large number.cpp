3dd5f67fc51401126ea980fd343f2a66384be76a
Roman Samgin <40915074+spprssr@users.noreply.github.com>
2020-08-21 16:04:49 +0300

    Add files via upload

---
+++ Last digit of a large number.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int last_digit(const std::string &str1, const std::string &str2) {
+  
+  if ((str1 == "0" || (str1 != "0")) && (str2 == "0")) return 1;
+  
+  int last2_str2(0);
+  if (str2.size() >= 2)
+  {
+      last2_str2 = (*(str2.end() - 2) - '0') * 10 + *(str2.end() - 1) - '0'; 
+  }
+  else
+  {
+      last2_str2 = *(str2.end() - 1) - '0';
+  }  
+  int last1_str1 = *(str1.end() - 1) - '0';
+  vector<int>ans{}; 
+  int  d = last1_str1;  
+  for(int i = 0; i < 4; ++i)
+  {
+      ans.push_back(d);
+      d = (d * last1_str1) % 10;
+  }  
+  if ((last2_str2 % 4) == 0)
+        return ans[3];
+  return ans[(last2_str2 % 4) - 1];
+}
