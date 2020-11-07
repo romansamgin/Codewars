54b909877a562623c2c788e05bd961a28595dbf2
Roman Samgin <40915074+romansamgin@users.noreply.github.com>
2020-11-07 22:49:24 +0300

    Update Battleship field validator.cpp

--- Battleship field validator.cpp
+++ Battleship field validator.cpp
@@ -1,3 +1,6 @@
+/////////////////////////////////////////////////////////////////
+//https://www.codewars.com/kata/52bb6539a4cf1b12d90005b7
+
 #include <iostream>
 #include <vector>
 #include <numeric>
