7fca633216e8e8bde537e56eed1837f28f4fdfd7
Roman Samgin <40915074+romansamgin@users.noreply.github.com>
2020-11-23 16:10:20 +0300

    Update Can you really count divisors.cpp

--- Can you really count divisors.cpp
+++ Can you really count divisors.cpp
@@ -2,10 +2,10 @@
 //https://www.codewars.com/kata/58b16300a470d47498000811
 
 #include <cmath>
+
 using namespace std;
 
-long double countDivisors(int n) 
-{
+long double countDivisors(int n) {
     long double sum = 0;
     long double sq = int(sqrt(n));
     for (int i = 1; i < sq + 1; ++i) {
